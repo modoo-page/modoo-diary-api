a0efb7599de5568b72949f246efb1e38221d5845
MINGKYME <admin@mingky.me>
2022-12-26 08:33:06 +0900

    ✨ token을 NULL로 초기화 할수있게 수정

--- database/update.go
+++ database/update.go
@@ -1,7 +1,9 @@
 package database
 
-func UpdateToken(email string, token string) (err error) {
-	tx := DB.Model(User{}).Where("email = ?", email).Update("auth_token", token)
+import "gopkg.in/guregu/null.v4"
+
+func UpdateToken(email string, token null.String, time null.Time) (err error) {
+	tx := DB.Model(User{}).Where("email = ?", email).Update("auth_token", token).Update("auth_expried_at", time)
 	err = tx.Error
 	return
 }
