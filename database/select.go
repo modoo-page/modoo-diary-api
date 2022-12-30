66570bb52cd37b32dea5035ae793b56d9929930a
MINGKYME <admin@mingky.me>
2022-12-30 23:41:34 +0900

    🐛 page가 1 줄어야하는 버그 수정

--- database/select.go
+++ database/select.go
@@ -17,7 +17,7 @@ func SelectDiaryListTop10() (diaryList []DiaryResponse, err error) {
 	return
 }
 func SelectDiaryListByPaging(page int, length int) (diaryList []DiaryResponse, err error) {
-	tx := DB.Table("diary").Joins("JOIN user ON user.user_id = diary.user_id").Order("diary_id DESC").Offset(page * length).Limit(length).Scan(&diaryList)
+	tx := DB.Table("diary").Joins("JOIN user ON user.user_id = diary.user_id").Order("diary_id DESC").Offset((page - 1) * length).Limit(length).Scan(&diaryList)
 	err = tx.Error
 	return
 }
